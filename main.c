95dbc3d6dc252a9f1045145ca7fecb1b10c6fd21
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-10-26 11:52:00 +0545

    defined an empty function commit() and moved functions around

--- main.c
+++ main.c
@@ -7,9 +7,82 @@ void navFolder();
 void setFilePath(GObject *source, GAsyncResult *res, gpointer user_data);
 void updateFilePath();
 
+void commit();
+
 //Global Variables
 char filepath[256];
 
+
+//Globalised stuff for initProject
+GtkWidget
+*windowInit,
+*entryDir;
+
+void initProject() {
+    GtkWidget
+    *gridParent,
+    *buttonNav,
+    *buttonProceed;
+
+    //Init of windowInit
+    windowInit = gtk_window_new();
+    gtk_window_set_title(GTK_WINDOW(windowInit),"Navigate to Project");
+    gtk_window_present(GTK_WINDOW(windowInit));
+    //Init of gridParent
+    gridParent = gtk_grid_new();
+    gtk_window_set_child(GTK_WINDOW(windowInit),gridParent);
+    //MArgins & Paddings
+    gtk_widget_set_margin_start(gridParent,10);
+    gtk_widget_set_margin_end(gridParent,10);
+    gtk_widget_set_margin_top(gridParent,10);
+    gtk_widget_set_margin_bottom(gridParent,10);
+    gtk_widget_set_halign(gridParent,GTK_ALIGN_CENTER);
+    gtk_widget_set_valign(gridParent,GTK_ALIGN_CENTER);
+
+    //Init of entryDir
+    entryDir = gtk_entry_new();
+    gtk_grid_attach(GTK_GRID(gridParent),entryDir,0,1,5,1);
+    //Margins& Paddings
+    gtk_widget_set_size_request(entryDir,250,-1);
+
+    //Init of buttonNav
+    buttonNav = gtk_button_new_with_label("🗀");
+    gtk_grid_attach(GTK_GRID(gridParent),buttonNav,5,1,1,1);
+    g_signal_connect(buttonNav,"clicked",G_CALLBACK(navFolder),NULL);
+
+    //Init of buttonProceed
+    buttonProceed = gtk_button_new_with_label("Proceed");
+    gtk_grid_attach(GTK_GRID(gridParent),buttonProceed,0,2,6,1);
+    g_signal_connect(buttonProceed,"clicked",G_CALLBACK(updateFilePath),NULL);
+
+
+}
+
+void navFolder() {
+    GtkWindow *windowNav = GTK_WINDOW(NULL);
+    GtkFileDialog *dialogNav = gtk_file_dialog_new();
+
+    GFile *home = g_file_new_for_path(g_get_home_dir());
+    gtk_file_dialog_set_initial_folder(dialogNav, home);
+    g_object_unref(home);
+    gtk_file_dialog_select_folder(dialogNav, windowNav, NULL,setFilePath,windowNav);
+}
+
+void setFilePath(GObject *source, GAsyncResult *res, gpointer user_data) {
+    //Fetches the file path
+    GtkFileDialog *dialogNav = GTK_FILE_DIALOG(source);
+    GFile *folder = gtk_file_dialog_select_folder_finish(dialogNav, res, NULL);
+    char *path = g_file_get_path(folder);
+    //Set the entryDir to the desired file path
+    gtk_editable_set_text(GTK_EDITABLE(entryDir),path);
+
+}
+
+void updateFilePath() {
+    //sets the filepath var to the file path in the entry
+    strcpy(filepath,gtk_editable_get_text(GTK_EDITABLE(entryDir)));
+    gtk_window_destroy(GTK_WINDOW(windowInit));
+}
 
 //Globalised Variables
     GtkWidget *windowMain;
@@ -88,76 +161,12 @@ static void activate(GtkApplication *app,gpointer user_data) {
 
 
 }
-//Globalised stuff for initProject
-GtkWidget
-*windowInit,
-*entryDir;
 
-void initProject() {
-    GtkWidget
-    *gridParent,
-    *buttonNav,
-    *buttonProceed;
-
-    //Init of windowInit
-    windowInit = gtk_window_new();
-    gtk_window_set_title(GTK_WINDOW(windowInit),"Navigate to Project");
-    gtk_window_present(GTK_WINDOW(windowInit));
-    //Init of gridParent
-    gridParent = gtk_grid_new();
-    gtk_window_set_child(GTK_WINDOW(windowInit),gridParent);
-    //MArgins & Paddings
-    gtk_widget_set_margin_start(gridParent,10);
-    gtk_widget_set_margin_end(gridParent,10);
-    gtk_widget_set_margin_top(gridParent,10);
-    gtk_widget_set_margin_bottom(gridParent,10);
-    gtk_widget_set_halign(gridParent,GTK_ALIGN_CENTER);
-    gtk_widget_set_valign(gridParent,GTK_ALIGN_CENTER);
-
-    //Init of entryDir
-    entryDir = gtk_entry_new();
-    gtk_grid_attach(GTK_GRID(gridParent),entryDir,0,1,5,1);
-    //Margins& Paddings
-    gtk_widget_set_size_request(entryDir,250,-1);
-
-    //Init of buttonNav
-    buttonNav = gtk_button_new_with_label("🗀");
-    gtk_grid_attach(GTK_GRID(gridParent),buttonNav,5,1,1,1);
-    g_signal_connect(buttonNav,"clicked",G_CALLBACK(navFolder),NULL);
-
-    //Init of buttonProceed
-    buttonProceed = gtk_button_new_with_label("Proceed");
-    gtk_grid_attach(GTK_GRID(gridParent),buttonProceed,0,2,6,1);
-    g_signal_connect(buttonProceed,"clicked",G_CALLBACK(updateFilePath),NULL);
-
+void commit() {
 
 }
 
-void navFolder() {
-    GtkWindow *windowNav = GTK_WINDOW(NULL);
-    GtkFileDialog *dialogNav = gtk_file_dialog_new();
 
-    GFile *home = g_file_new_for_path(g_get_home_dir());
-    gtk_file_dialog_set_initial_folder(dialogNav, home);
-    g_object_unref(home);
-    gtk_file_dialog_select_folder(dialogNav, windowNav, NULL,setFilePath,windowNav);
-}
-
-void setFilePath(GObject *source, GAsyncResult *res, gpointer user_data) {
-    //Fetches the file path
-    GtkFileDialog *dialogNav = GTK_FILE_DIALOG(source);
-    GFile *folder = gtk_file_dialog_select_folder_finish(dialogNav, res, NULL);
-    char *path = g_file_get_path(folder);
-    //Set the entryDir to the desired file path
-    gtk_editable_set_text(GTK_EDITABLE(entryDir),path);
-
-}
-
-void updateFilePath() {
-    //sets the filepath var to the file path in the entry
-    strcpy(filepath,gtk_editable_get_text(GTK_EDITABLE(entryDir)));
-    gtk_window_destroy(GTK_WINDOW(windowInit));
-}
 
 
 
